f5b5af8dd5228f0eb2945ff59ab299f6bd1ce486
NIYIGIRA salomon <53565906+niysalomon@users.noreply.github.com>
2021-11-27 12:43:44 +0200

    updating port

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-// const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
-app.listen(4001,()=>{
+app.listen(PORT,()=>{
     console.log("backend wll connected!!");
 }); 
 
